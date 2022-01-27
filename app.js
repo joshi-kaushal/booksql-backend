b0520b67880cfb16cb0d0db712350f9d7c650164
7JKaushal <7joshikaushal@gmail.com>
2022-01-27 11:25:11 +0530

    chaged __dirname according to es6 syntax

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import mongoose from "mongoose"
 import cors from "cors"
-import path from "path"
 import schema from "./schema/schema.js";
+
+// Using ES6 syntax in Node so __dirname is not available out of the box
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // MongoDB Connection
 mongoose.connect("mongodb+srv://kaushal:booksql@cluster0.9ldef.mongodb.net/books?retryWrites=true&w=majority")
@@ -12,13 +17,16 @@ mongoose.connection.once("open", () => console.log(`connected to database!`))
 const app = express()
 app.use(cors())
 
+// PORT
 const PORT = process.env.PORT || 5000
 
+// GraphQL
 app.use("/graphql", graphqlHTTP({
 	schema,
 	graphiql: true
 }))
 
+// Deployment
 if(process.env.NODE_ENV === "production") {
 	app.use(express.static("../client/build"))
 	app.get("/", (req, res) => {
@@ -26,6 +34,7 @@ if(process.env.NODE_ENV === "production") {
 	})
 }
 
+// Listening on port PORT || 5000
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
 })
