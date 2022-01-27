011576d05efe6b43577d63b547cf496d91444e67
7JKaushal <7joshikaushal@gmail.com>
2022-01-27 10:31:30 +0530

    another try

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use("/graphql", graphqlHTTP({
 }))
 
 if(process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"))
+	app.use(express.static("../client/build"))
 	app.get("/", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
 	})
