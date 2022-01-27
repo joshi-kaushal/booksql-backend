64192d21320da991914b01838b17dc2d2b3f921b
7JKaushal <7joshikaushal@gmail.com>
2022-01-27 12:13:45 +0530

    added /build from client

--- app.js
+++ app.js
@@ -28,10 +28,10 @@ app.use("/graphql", graphqlHTTP({
 
 // Deployment
 if(process.env.NODE_ENV === "production") {
-	app.use(express.static("client"))
-	app.get("/", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-	})
+	app.use(express.static(path.resolve(__dirname, "./build")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "./build", "index.html"));
+	});
 }
 
 // Listening on port PORT || 5000
